fd87ab9fef2c73f673b33cb7c32c7e2250df103e
Titouan <titouan.galvani@laposte.net>
2018-07-20 23:55:04 +0200

    Filling methods with their properties :books:

--- src/TodoStore.ts
+++ src/TodoStore.ts
@@ -1,26 +1,39 @@
 import {Todo} from './Interfaces'
 
-class TodoStore {
+export default class TodoStore {
 
+  private static i = 0
   public todos: Todo[]
 
+  static increment () {
+    return this.i++
+  }
+
   addTodo (title: string): void {
-
+    this.todos = [{
+      id: 0,
+      title: title,
+      completed: false
+    }, ...this.todos]
   }
 
-  removeTodo (todo): void {
-
+  removeTodo (todo: Todo): void {
+    this.todos = this.todos.filter(t => t !== todo)
   }
 
-  toggleTodo (todo, completed = true): void {
-
+  toggleTodo (todo: Todo, completed = true): void {
+    this.todos = this.todos.map(t => t === todo ? { ...t, completed } : t)
   }
 
-  editTodo (todo, title): void {
+  editTodo (todo: Todo, title: string): void {
+    this.todos = this.todos.map(t => t === todo ? { ...t, title } : t)
+  }
 
+  toggleAll (completed = true) {
+    this.todos = this.todos.map(t => completed !== t.completed ? { ...t, completed } : t)
   }
 
   cleanTodo (): void {
-
+    this.todos = this.todos.filter(t => !t.completed)
   }
 }
