679e36d00c04d39304b7a30a233e85f8891cc6b4
Titouan <titouan.galvani@laposte.net>
2018-07-20 23:43:11 +0200

    Adding TodoStore and created Interface :raised_hand:

---
+++ src/TodoStore.ts
@@ -0,0 +1,26 @@
+import {Todo} from './Interfaces'
+
+class TodoStore {
+
+  public todos: Todo[]
+
+  addTodo (title: string): void {
+
+  }
+
+  removeTodo (todo): void {
+
+  }
+
+  toggleTodo (todo, completed = true): void {
+
+  }
+
+  editTodo (todo, title): void {
+
+  }
+
+  cleanTodo (): void {
+
+  }
+}
